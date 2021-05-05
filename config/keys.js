c0c431df68fa1ad61bc13b520b068d62aa55818d
David Hao <davidjhao2000@gamil.com>
2021-05-05 00:15:47 -0400

    attempt 3

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@ module.exports = {
 		'mongodb+srv://DavidHao:Henniker2001@cluster0.cn3cg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 	client_id     : '5e80c27d675d40609c8fb7b68eeae49a',
 	client_secret : '50da151d6ebc4220b0574a75c994d8d6',
-	redirect_uri  : `http://localhost:5000/api/login/callback`,
+	redirect_uri  : `https://collager-426.herokuapp.com/api/login/callback`,
 	scopes        : 'user-top-read',
 	url           : 'http://localhost:3000'
 };
